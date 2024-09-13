b2c122e335d67e34fcddf76fb09d9c65b44c0942
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2024-09-13 10:34:30 +0300

    refactor

--- SDL/app.c
+++ SDL/app.c
@@ -1,11 +1,16 @@
 #include "sim.h"
 
+#define RANDOM_GENERATION 5
+
 int get_neigbours_count(int board[SIM_Y_SIZE][SIM_X_SIZE], int y, int x) {
-  static const int dirs[8][2] = { {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1} };
+  static const int dirs[8][2] = {{1, 0},  {1, 1},   {0, 1},  {-1, 1},
+                                 {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
   int count = 0;
   for (int i = 0; i < 8; i++) {
-    int dy = y + dirs[i][0] < 0 ? y + dirs[i][0] + SIM_Y_SIZE : (y + dirs[i][0]) % SIM_Y_SIZE;
-    int dx = x + dirs[i][1] < 0 ? x + dirs[i][1] + SIM_X_SIZE : (x + dirs[i][1]) % SIM_X_SIZE;
+    int dy = y + dirs[i][0] < 0 ? y + dirs[i][0] + SIM_Y_SIZE
+                                : (y + dirs[i][0]) % SIM_Y_SIZE;
+    int dx = x + dirs[i][1] < 0 ? x + dirs[i][1] + SIM_X_SIZE
+                                : (x + dirs[i][1]) % SIM_X_SIZE;
     if (board[dy][dx]) {
       ++count;
     }
@@ -14,12 +19,12 @@ int get_neigbours_count(int board[SIM_Y_SIZE][SIM_X_SIZE], int y, int x) {
 }
 
 void app() {
-  int board_last[SIM_Y_SIZE][SIM_X_SIZE] = {{ 0 }};
-  int board[SIM_Y_SIZE][SIM_X_SIZE] = {{ 0 }};
+  int board_last[SIM_Y_SIZE][SIM_X_SIZE] = {{0}};
+  int board[SIM_Y_SIZE][SIM_X_SIZE] = {{0}};
 
   for (int y = 0; y < SIM_Y_SIZE; ++y) {
     for (int x = 0; x < SIM_X_SIZE; ++x) {
-      if (simRand() % 5 == 0) {
+      if (simRand() % RANDOM_GENERATION == 0) {
         board_last[y][x] = 1;
       }
     }
