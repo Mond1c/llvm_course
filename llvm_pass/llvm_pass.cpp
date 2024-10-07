4486c0e3b14c29d804d1a5083aaa9b8a235bae74
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2024-10-07 17:24:23 +0300

    first version

--- llvm_pass/llvm_pass.cpp
+++ llvm_pass/llvm_pass.cpp
@@ -1,4 +1,5 @@
 #include <unordered_map>
+#include <algorithm>
 #include <string>
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Module.h"
@@ -18,22 +19,66 @@ struct InstructionTracerModPass : public PassInfoMixin<InstructionTracerModPass>
             return PreservedAnalyses::all();
         }
         std::unordered_map<std::string, int> stats;
-        
+        std::vector<std::string> instructions;
+
         for (auto& F : M) {
             if (F.isDeclaration()) continue;
             for (auto& B : F) {
                 for (auto& I : B) {
                     if (!isa<PHINode>(&I)) {
                         stats[I.getOpcodeName()]++;
+                        instructions.push_back(I.getOpcodeName());
                     }
                 }
             }
         }
 
+        std::vector<std::pair<std::string, int>> s(stats.begin(), stats.end());
+        std::sort(s.begin(), s.end(), [](const auto& lhs, const auto& rhs) {
+                return lhs.second > rhs.second;
+                });
+
         outs() << "[STATS]" << "\n";
-        for (const auto& item : stats) {
+        for (const auto& item : s) {
             outs() << item.first << ": " << item.second << "\n";
         }
+
+        // Patterns
+        
+        std::unordered_map<std::string, int> patterns;
+
+        for (int i = 0; i < instructions.size(); i++) {
+            std::string instr = instructions[i];
+            patterns[instr]++;
+
+            if (i > 0) {
+                instr = instructions[i - 1] + " " + instr;
+                patterns[instr]++;
+            }
+            if (i - 1 > 0) {
+                instr = instructions[i - 2] + " " + instr;
+                patterns[instr]++;
+            }
+            if (i - 2 > 0) {
+                instr = instructions[i - 3] + " " + instr;
+                patterns[instr]++;
+            }
+            if (i - 3 > 0) {
+                instr = instructions[i - 4] + " " + instr;
+                patterns[instr]++;
+            }
+        }
+
+        std::vector<std::pair<std::string, int>> p(patterns.begin(), patterns.end());
+        std::sort(p.begin(), p.end(), [](const auto& lhs, const auto& rhs) {
+                return lhs.second > rhs.second;
+        });
+
+        outs() << "[PATTERNS]" << "\n";
+        for (const auto& item : p) {
+            outs() << item.first << ": " << item.second << "\n";
+        }
+
 
         return PreservedAnalyses::all();
     }
